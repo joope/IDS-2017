79a343896aa5021b09bef85cea8ed3caf69646cc
joope <pyry.joona@helsinki.fi>
2017-10-16 20:55:39 +0300

    Send motion data to api server

--- Image Processing/motiondetect.py
+++ Image Processing/motiondetect.py
@@ -3,79 +3,131 @@ import sys
 import time
 import numpy
 import urllib.request
-import requests
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 
-red = (100,0,0)
-blur = 5
+br = 5
 kernel = numpy.ones((5,5), numpy.uint8)
+interval = 10
 
-api = 'http://siika.es:1337'
-
+api = 'http://siika.es:1337/motion'
+prev = []
+new = []
 
 sources = [
-	'http://haba.tko-aly.fi/kuvat/webcam1.jpg',
-	'http://haba.tko-aly.fi/kuvat/webcam2.jpg'
+    'http://haba.tko-aly.fi/kuvat/webcam1.jpg',
+    'http://haba.tko-aly.fi/kuvat/webcam2.jpg'
 ]
 
 seatCoordinates = [
-	(960, 400), 
-	(800, 500), 
-	(610, 595), 
-	(415, 745), 
-	(600, 915), 
-	(750, 1060), 
-	(1340, 1030), 
-	(1565, 800), 
-	(1735, 615)]
-	
-def sendData(data):
-	r = requests.post(api, data=data, allow_redirects=True)
-	print r.content
+    (960, 400), 
+    (800, 500), 
+    (610, 595), 
+    (415, 745), 
+    (600, 915), 
+    (750, 1060), 
+    (1340, 1030), 
+    (1565, 800), 
+    (1735, 615)]
 
+# Read image from url as greyscale
 def getImageFromUrl(url):
-	res = urllib.request.urlopen(url)
-	img = numpy.asarray(bytearray(res.read()), dtype="uint8")
-	img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
+    res = urllib.request.urlopen(url)
+    img = numpy.asarray(bytearray(res.read()), dtype="uint8")
+    img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
 
-	return img
+    return img
 
 def checkSofas(img):
-	for i, c in enumerate(seatCoordinates):
-		x,y = c
-		size = 20
-		roi = img[y-size:y+size, x-size:x+size]
-		mean = roi.mean()
-		print("%s: %s" % (i, mean > 100))
-		cv2.rectangle(img, (x-size, y-size), (x+size, y+size), red)
+    for i, c in enumerate(seatCoordinates):
+        x,y = c
+        size = 20
+        roi = img[y-size:y+size, x-size:x+size]
+        mean = roi.mean()
+        print("%s: %s" % (i, mean > 100))
+        cv2.rectangle(img, (x-size, y-size), (x+size, y+size), red)
 
-def compareImages(img1, img2):
-	#blur1 = cv2.blur(img1, (10,10))
-	#blur2 = cv2.blur(img2, (10,10))
-	#diff = cv2.absdiff(blur1, blur2)
-	diff = cv2.absdiff(img1, img2)
-	ret, thresh = cv2.threshold(diff, 5, 255, cv2.THRESH_BINARY)
-	#checkSofas(thresh)
-	#dilated = cv2.dilate(thresh, kernel, iterations=2)
+def showImageAndWait(img):
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
 
-	max = diff.shape[0]*diff.shape[1]*255
-	print(numpy.sum(diff)/max) # Skaala: 200 000 - 50 000 000
-	cv2.imshow('image', diff)
-	cv2.waitKey(0)
-	
-	sendData()
-	#cv2.imshow('image', thresh)
-	#cv2.waitKey(0)
-	#showImage(blur)
-	#cv2.imshow('image', thresh)
-	#cv2.waitKey(0)
+def compareImages(prev, new):
 
-prev1 = getImageFromUrl(sources[1])
-#prev1 = cv2.imread('imageset/room1_clear.jpg', 0)
+    img1 = prev
+    img2 = cv2.blur(new, (br,br))
+        
+    diff = cv2.absdiff(img1, img2)
+    ret, thresh = cv2.threshold(diff, 5, 255, cv2.THRESH_BINARY)
 
-for i in range(0,10):
-	time.sleep(5)
-	new1 = getImageFromUrl(sources[1])
-	blur1 = cv2.blur(new1, (blur,blur))
-	compareImages(blur1, prev1)
-	prev1 = cv2.addWeighted(prev1, 0.5, blur1, 0.5, 0)
+    # Absolute change in pixel values
+    # Usually between 200 000 - 50 000 000
+    absolutePixelChange = numpy.sum(diff)
+
+    # Calculate maximum pixel values from image size
+    maxPixelValue = diff.shape[0]*diff.shape[1]*255
+
+    # Percentage of change relative to maximum change in pixels
+    # If all pixels change from 0 -> 255, the value is 1,0
+    relativePixelChange = absolutePixelChange / maxPixelValue
+
+    # Percentage of change in thresholded image
+    thresholdChange = numpy.sum(thresh)
+    relativeThresholdChange = thresholdChange / maxPixelValue
+
+    # Uncomment to view the images:
+    showImageAndWait(diff)
+    # showImageAndWait(thresh)
+
+    # Discard data if change is too much (i.e light switched off)
+    if(relativeThresholdChange >= 0.80):
+        return None
+
+    data = {
+        'abs_change': absolutePixelChange,
+        'rel_change': relativePixelChange,
+        'thresh_change': relativeThresholdChange
+    }
+
+    return data
+
+def processImages(prev, new):
+    processed = []
+    for i, img in enumerate(new):
+        blurred = cv2.blur(img, (br,br))
+        processed.append(cv2.addWeighted(prev[i], 0.5, blurred, 0.5, 0))
+
+    return processed
+
+def sendData(data, location):
+    if(data):
+        data['location'] = location
+        request = Request(api, urlencode(data).encode())
+        json = urlopen(request).read().decode()
+        print(json)
+
+def fetchImages():
+    images = []
+    for cam in sources:
+        images.append(getImageFromUrl(cam))
+        
+    return images
+
+
+def mainLoop(interval, maxIterations=1000):
+    prev = fetchImages()
+    i = 0
+    while i < maxIterations:
+        time.sleep(interval)
+        new = fetchImages()
+        data1 = compareImages(prev[0], new[0])
+        data2 = compareImages(prev[1], new[1])
+        sendData(data1, 0)
+        sendData(data2, 1)
+        prev = processImages(prev, new)
+              
+        i += 1
+
+mainLoop(10)
+
+
             
